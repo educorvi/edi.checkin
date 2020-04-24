cbdc3be85118cfe3581f817ffaccb815d84f59a5
Lars Walther <meinkraeks@gmail.com>
2020-04-24 23:44:24 +0200

    "Add view: check-checkin"

--- src/edi/checkin/views/check_checkin.py
+++ src/edi/checkin/views/check_checkin.py
@@ -6,14 +6,12 @@ from Products.Five.browser import BrowserView
 # from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
 
-class CheckedMessage(BrowserView):
+class CheckCheckin(BrowserView):
     # If you want to define a template here, please remove the template from
     # the configure.zcml registration of this view.
-    # template = ViewPageTemplateFile('checked_message.pt')
+    # template = ViewPageTemplateFile('check_checkin.pt')
 
     def __call__(self):
         # Implement your own actions:
-        self.status = self.request.get('status')
-        self.cssclass = self.request.get('class')
-        self.date = self.request.get('date')
+        self.msg = _(u'A small message')
         return self.index()
