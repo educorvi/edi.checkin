c07d13813549ffdf7766758fabfc19546bc54cf9
Lars Walther <meinkraeks@gmail.com>
2020-04-25 21:18:21 +0200

    Content-Type Office finished

--- src/edi/checkin/content/office.py
+++ src/edi/checkin/content/office.py
@@ -1,60 +1,34 @@
 # -*- coding: utf-8 -*-
-# from plone.app.textfield import RichText
-# from plone.autoform import directives
 from plone.dexterity.content import Container
-# from plone.namedfile import field as namedfile
 from plone.supermodel import model
-# from plone.supermodel.directives import fieldset
-# from z3c.form.browser.radio import RadioFieldWidget
-# from zope import schema
+from zope import schema
 from zope.interface import implementer
 
 
-# from edi.checkin import _
+from edi.checkin import _
 
 
 class IOffice(model.Schema):
     """ Marker interface and Dexterity Python Schema for Office
     """
-    # If you want, you can load a xml model created TTW here
-    # and customize it in Python:
 
-    # model.load('office.xml')
+    adressen = schema.List(title=_(u"Gültige E-Mail-Adressen für diesen Raum"),
+                           description=_(u"Hier legen Sie fest, welche Adressen für diesen Raum einchecken dürfen."),
+                           value_type=schema.TextLine(),
+                           required=True)
 
-    # directives.widget(level=RadioFieldWidget)
-    # level = schema.Choice(
-    #     title=_(u'Sponsoring Level'),
-    #     vocabulary=LevelVocabulary,
-    #     required=True
-    # )
+    beginn = schema.TextLine(title=_(u"Frühester Arbeitsbeginn in diesem Office (Format: HH:MM)"),
+                             required=True)
 
-    # text = RichText(
-    #     title=_(u'Text'),
-    #     required=False
-    # )
+    ende = schema.TextLine(title=_(u"Spätestes Arbeitsende in diesem Office (Format: HH:MM)"),
+                           required=True)
 
-    # url = schema.URI(
-    #     title=_(u'Link'),
-    #     required=False
-    # )
+    maxtime = schema.Int(title=_(u"Maximale Aufenthaltsdauer pro Person in diesem Office (Format: H)"),
+                           required=True)
 
-    # fieldset('Images', fields=['logo', 'advertisement'])
-    # logo = namedfile.NamedBlobImage(
-    #     title=_(u'Logo'),
-    #     required=False,
-    # )
+    maxperson = schema.Int(title=_(u"Maximale Bürobesetzung in Personen an Arbeitstagen"),
+                           required=True)
 
-    # advertisement = namedfile.NamedBlobImage(
-    #     title=_(u'Advertisement (Gold-sponsors and above)'),
-    #     required=False,
-    # )
-
-    # directives.read_permission(notes='cmf.ManagePortal')
-    # directives.write_permission(notes='cmf.ManagePortal')
-    # notes = RichText(
-    #     title=_(u'Secret Notes (only for site-admins)'),
-    #     required=False
-    # )
 
 
 @implementer(IOffice)
