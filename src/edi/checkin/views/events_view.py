6dff39464af728acc5c41855719a5e41b6bc3b7c
lwalther <lwalther@novareto.de>
2020-05-11 09:11:24 +0200

    event view for offices

--- src/edi/checkin/views/events_view.py
+++ src/edi/checkin/views/events_view.py
@@ -1,17 +1,39 @@
 # -*- coding: utf-8 -*-
-
+import datetime
 from edi.checkin import _
 from Products.Five.browser import BrowserView
-
+from plone import api as ploneapi
 # from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
 
 class EventsView(BrowserView):
-    # If you want to define a template here, please remove the template from
-    # the configure.zcml registration of this view.
-    # template = ViewPageTemplateFile('events_view.pt')
+
+
+    def get_upcoming_veranstaltungen(self):
+        
+        entries = []
+        eventquery = {'query': (datetime.datetime.now()), 'range':'min'}
+        contents = ploneapi.content.find(context=self.context, portal_type="Veranstaltung", 
+                                         start=eventquery, sort_on="start", sort_order="ascending")
+        for i in contents:
+            entry = {}
+            entry['title'] = i.Title
+            entry['description'] = i.Description
+            entry['url'] = i.getURL()
+            entry['day'] = i.start.strftime('%d.%m.')
+            entry['time'] = "%s - %s" %(i.start.strftime('%H:%M'), i.end.strftime('%H:%M'))
+            if datetime.date.today() == i.start.date():
+                entry['btnclass'] = u'btn btn-primary btn-lg'
+                entry['comment'] = u''
+            else:
+                entry['btnclass'] = u'btn btn-primary btn-lg disabled'
+                entry['comment'] = u'Ein Checkin ist aus Gründen des Infektionsschutzes erst am Tag der Veranstaltung möglich'
+            entries.append(entry)
+        return entries
+            
 
     def __call__(self):
+
         # Implement your own actions:
-        self.msg = _(u'A small message')
+        self.veranstaltungen = self.get_upcoming_veranstaltungen()
         return self.index()
